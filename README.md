c1284c968b7cc23dbed60385b3745b3ba00f9e04
Dominic DiModugno <d.DiModugno@acilab.com>
2024-08-15 15:03:00 -0500

    split code into partials and added helper functions

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Project Name: Workopia
 ## Description: 
-A fake job listing website where users can view job ads, post job ads, and apply for jobs. 
+A job listing website where users can view job ads, post job ads, and apply for jobs. 
 
 ## Notes: 
-Developed Locally on XAMPP, Appache server configureations were changed
-document root in httpd.conf was set to ```/Workopia/public``` so that the 
-project appears when you go to localhost:3030. 
+Developed Locally on XAMPP, Appache server configureations were changed.
+Document root in httpd.conf was set to ```/project_name/public``` so that the Appache looks for the index in the public folder when you go to localhost in your browser. 
--- helpers.php
+++ helpers.php
@@ -5,7 +5,64 @@
  * 
  * @param string path
  * @return string
+ * 
  */
 function basePath($path = '') {
-  return __DIR__ . '/' . $path;
+  return __DIR__ . '\\' . $path;
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadView($name) {
+  $viewPath = basePath("views\\" . $name . ".view.php");
+  if (file_exists($viewPath)) {
+    require $viewPath;
+  } else {
+    echo "View '${name}' not found!";
+  }
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+function loadPartial($name) {
+  $partialPath = basePath("views\partials\\" . $name . ".php");
+  if (file_exists($partialPath)) {
+    require $partialPath;
+  } else {
+    echo "Partial '${name}' not found!";
+  }
+}
+
+/**
+ * Preformat text - will inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function pr($value) {
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Preformat text and exit
+ * @param mixed $value
+ * @return void
+ */
+function pre($value) {
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+  die();
 }
