01c1c07608b1d590e216e8771bdc7648f6ce36c0
Dominic DiModugno <ddimodugno@outlook.com>
2024-09-09 12:36:24 -0500

    handling route params

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -23,7 +23,7 @@ class ListingController
   {
     $params = [];
     $listings = $this->db->query('SELECT * FROM listings', $params)->fetchAll();
-    loadView('home', [
+    loadView('listings/index', [
       'listings' => $listings,
     ]);
   }
@@ -43,14 +43,21 @@ class ListingController
    * 
    * @return void
    */
-  public function show()
+  public function show($params)
   {
 
-    $id = $_GET['id'] ?? '';
+    $id = $params['id'] ?? '';
     $params = [
       'id' => $id
     ];
     $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    // Check if listing exists
+    if(!$listing) {
+      ErrorController::notFound('Listing not found'); 
+      return;
+    }
+
     loadView('listings/show', [
       'listing' => $listing
     ]);
