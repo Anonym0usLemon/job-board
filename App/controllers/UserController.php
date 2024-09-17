87c42e12bf665ada2780399d353f976fa01ad954
Dominic DiModugno <ddimodugno@outlook.com>
2024-09-17 13:47:57 -0500

    set session

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -2,6 +2,7 @@
 namespace App\Controllers;
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session; 
 
 class UserController
 {
@@ -103,6 +104,17 @@ class UserController
 
     $this->db->query('INSERT INTO users (name, email, city, state, password)
     VALUES (:name, :email, :city, :state, :password)', $params);
+
+    // Get new user ID
+    $userId = $this->db->conn->lastInsertId(); 
+
+    Session::set('user', [
+      'id' => $userId,
+      'name' => $_POST['name'],
+      'email' => $_POST['email'],
+      'city' => $_POST['city'],
+      'state' => $_POST['state'],
+    ]);
 
     redirect('/');
   }
